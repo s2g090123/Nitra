3a37c265f30288301ea4712996f1aef97d86f54b
s2g090123 <s2g090123@gmail.com>
2025-03-05 16:01:55 +0800

    Refactor: Add UI test and custom test runner

---
+++ app/src/androidTest/java/com/jiachian/nitra/MainActivityTest.kt
@@ -0,0 +1,31 @@
+package com.jiachian.nitra
+
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@HiltAndroidTest
+class MainActivityTest {
+    @get:Rule
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    @Before
+    fun setup() {
+        hiltRule.inject()
+    }
+
+    @Test
+    fun test_entire_app() {
+        composeTestRule
+            .onNodeWithText("Add Your First Card")
+            .performClick()
+    }
+}
